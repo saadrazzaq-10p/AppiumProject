1a414814d56b70830b6dd9a88e6f1819d51a3d0a
muhammad.razzaq-at-022587608743 <muhammad.razzaq@edetek.com>
2023-03-24 22:00:14 +0500

    Appium Project with 2 assignments

---
+++ src/main/java/baseClass.java
@@ -0,0 +1,27 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import reader.configFileReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Test
+public class baseClass {
+    public static AppiumDriver driver;
+
+    public static reader.configFileReader configFileReader;
+
+    public void start() throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        configFileReader = new reader.configFileReader();
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, configFileReader.getdeviceName());
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, configFileReader.getplatformName());
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, configFileReader.getplatformVersion());
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, configFileReader.getautomationName());
+        cap.setCapability(MobileCapabilityType.TIMEOUTS, "10000");
+        cap.setCapability(MobileCapabilityType.APP, configFileReader.getappPath());
+        URL url = new URL(configFileReader.getappiumServerURL());
+        driver = new AppiumDriver(url, cap);
+    }
+}
