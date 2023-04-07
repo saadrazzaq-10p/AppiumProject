87db776e7b46f8b22793de4b81207310588aaf66
muhammad.razzaq-at-022587608743 <muhammad.razzaq@edetek.com>
2023-04-07 13:55:12 +0500

    Updated with DD approach

---
+++ src/main/java/base/appDriver.java
@@ -0,0 +1,15 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+
+public class appDriver {
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void setDriver(WebDriver Driver) {
+        driver.set(Driver);
+    }
+}
